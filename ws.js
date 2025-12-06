f5d1eb912961186be43aee8a44b8822fc27b564c
IOTVZLA2025 <thing11025@gmail.com>
2025-12-05 22:28:02 -0400

    Add files via upload

---
+++ ws.js
@@ -0,0 +1,12 @@
+// sw.js
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  // Por ahora no hacemos caché; solo dejamos pasar las peticiones.
+});
